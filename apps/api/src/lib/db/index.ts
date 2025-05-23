87cd7a1ac271c7050854e09e9047ff79c2acbdb8
ZerNico <nico.franke01@gmail.com>
2025-05-23 17:16:03 +0200

    feat(db): enhance migration process with logging and error handling

--- apps/api/src/lib/db/index.ts
+++ apps/api/src/lib/db/index.ts
@@ -3,18 +3,23 @@ import { drizzle } from "drizzle-orm/bun-sql";
 import { migrate } from "drizzle-orm/bun-sql/migrator";
 import { env } from "../../config/env";
 import { advisoryLock } from "../../utils/db";
+import { tryCatch } from "../../utils/try-catch";
+import { logger } from "../logger";
 import { relations } from "./relations";
 import * as schema from "./schema";
 
+logger.info("Connecting to database");
 const client = new SQL(env.POSTGRES_URL);
 export const db = drizzle({ client, schema, relations });
 export type DB = typeof db;
 
 const MIGRATION_LOCK_ID = 3898613124;
-const runMigration = async () => {
+const runMigrations = async () => {
+  logger.info("Running migrations");
   const unlock = await advisoryLock(db, MIGRATION_LOCK_ID);
-  await migrate(db, { migrationsFolder: "drizzle" });
+  await tryCatch(migrate(db, { migrationsFolder: "drizzle" }));
   await unlock();
+  logger.info("Migrations completed");
 };
 
-await runMigration();
+await runMigrations();
