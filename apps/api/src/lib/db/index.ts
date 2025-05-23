07ebae8dc3a5a131f9df71a7478ef3d277553efd
ZerNico <nico.franke01@gmail.com>
2025-05-23 17:51:43 +0200

    refactor(db): dont lock migrations for now

--- apps/api/src/lib/db/index.ts
+++ apps/api/src/lib/db/index.ts
@@ -2,8 +2,6 @@ import { SQL } from "bun";
 import { drizzle } from "drizzle-orm/bun-sql";
 import { migrate } from "drizzle-orm/bun-sql/migrator";
 import { env } from "../../config/env";
-import { advisoryLock } from "../../utils/db";
-import { tryCatch } from "../../utils/try-catch";
 import { logger } from "../logger";
 import { relations } from "./relations";
 import * as schema from "./schema";
@@ -13,13 +11,4 @@ const client = new SQL(env.POSTGRES_URL);
 export const db = drizzle({ client, schema, relations });
 export type DB = typeof db;
 
-const MIGRATION_LOCK_ID = 3898613124;
-const runMigrations = async () => {
-  logger.info("Running migrations");
-  const unlock = await advisoryLock(db, MIGRATION_LOCK_ID);
-  await tryCatch(migrate(db, { migrationsFolder: "drizzle" }));
-  await unlock();
-  logger.info("Migrations completed");
-};
-
-await runMigrations();
+await migrate(db, { migrationsFolder: "drizzle" });
