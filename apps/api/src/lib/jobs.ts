37866f59f605d064e6cf71b73d05e79978c00878
ZerNico <nico.franke01@gmail.com>
2025-05-23 10:24:12 +0200

    refactor(api): remove old cleanup service

---
+++ apps/api/src/lib/jobs.ts
@@ -0,0 +1,7 @@
+import { setupTokenCleanupJob } from "../auth/jobs";
+import { setupLobbyCleanupJob } from "../lobby/jobs";
+
+export function setupJobs() {
+  setupLobbyCleanupJob();
+  setupTokenCleanupJob();
+}
--- apps/api/src/lib/jobs/index.ts
+++
@@ -1,7 +0,0 @@
-import { setupTokenCleanupJob } from "../../auth/jobs";
-import { setupLobbyCleanupJob } from "../../lobby/jobs";
-
-export function setupJobs() {
-  setupLobbyCleanupJob();
-  setupTokenCleanupJob();
-}
