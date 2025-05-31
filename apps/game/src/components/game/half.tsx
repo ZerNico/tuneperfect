be50e89f743e8fffc56e7b150c7f89cfe25ed3d6
ZerNico <nico.franke01@gmail.com>
2025-05-31 17:34:15 +0200

    feat(game): hide lyrics for long gaps

--- apps/game/src/components/game/half.tsx
+++ apps/game/src/components/game/half.tsx
@@ -1,5 +1,7 @@
-import { Show } from "solid-js";
+import { Show, createEffect, createSignal } from "solid-js";
+import { useGame } from "~/lib/game/game-context";
 import { createPlayer } from "~/lib/game/player";
+import { beatToMs } from "~/lib/ultrastar/bpm";
 import Avatar from "../ui/avatar";
 import Lyrics from "./lyrics";
 import Pitch from "./pitch";
@@ -10,21 +12,55 @@ interface HalfProps {
 }
 
 export default function Half(props: HalfProps) {
-  const { PlayerProvider, player } = createPlayer(() => ({
+  const { PlayerProvider, player, phrase } = createPlayer(() => ({
     index: props.index,
   }));
+  const game = useGame();
+
+  const [shouldHide, setShouldHide] = createSignal(false);
+
+  createEffect(() => {
+    const p = phrase();
+    const song = game.song();
+    if (!p || !song || !game.started()) {
+      setShouldHide(false);
+      return;
+    }
+
+    const phraseStartBeat = p.notes[0]?.startBeat;
+
+    if (phraseStartBeat === undefined) {
+      setShouldHide(false);
+      return;
+    }
+
+    const currentTimeMs = game.ms();
+    const phraseStartMs = beatToMs(song, phraseStartBeat);
+
+    const timeUntilPhraseMs = phraseStartMs - currentTimeMs;
+
+    if (timeUntilPhraseMs > 20000) {
+      setShouldHide(true);
+    } else if (timeUntilPhraseMs <= 10000) {
+      setShouldHide(false);
+    }
+  });
 
   return (
     <PlayerProvider>
-      <div
-        class="relative flex"
-        classList={{
-          "flex-col": props.index === 0,
-          "flex-col-reverse": props.index === 1,
-        }}
-      >
-        <Lyrics />
-        <Pitch />
+      <div class="relative">
+        <div
+          class="relative flex h-full w-full flex-col"
+          classList={{
+            "flex-col": props.index === 0,
+            "flex-col-reverse": props.index === 1,
+            "opacity-0": shouldHide(),
+            "transition-opacity duration-2000": !shouldHide(),
+          }}
+        >
+          <Lyrics />
+          <Pitch />
+        </div>
         <div
           class="absolute right-0 left-0 flex items-center justify-between px-20 py-4"
           classList={{
